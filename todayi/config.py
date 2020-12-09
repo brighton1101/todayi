efaed0e6fc086273c34d0655f9d9ad20adfe0fa6
Brighton Balfrey <balfrey@usc.edu>
2020-12-09 14:50:38 -0800

    Updated config

--- todayi/config.py
+++ todayi/config.py
@@ -1,6 +1,73 @@
-class Config:
-    DEFAULT_PATH = "~/todayi"
+"""
+Module used to interact with configuration file.
+By default, if configuration file does not exist
+the default config is written to a file at
+the specified CONFIG_PATH within users' home
+directory.
+"""
+
+import json
+from typing import Any
+
+from todayi.util.fs import path, file_text, write_file
 
 
-def load() -> Config:
-    pass
+CONFIG_PATH = '~/todayi.config'
+
+
+DEFAULT_CONFIG = {
+    'backend': 'sqlite',
+    'backend_dir': '~/todayi/',
+    'remote': 'GCS',
+    'remote_address': None
+}
+
+
+file_path = path(CONFIG_PATH)
+
+
+def _check_key(key):
+    if key not in DEFAULT_CONFIG.keys():
+        raise IndexError('Config: {} is not valid'.format(key))
+
+
+def _read_config():
+    return json.loads(file_text(file_path))
+
+
+def _write_config(config):
+    return write_file(json.dumps(config), file_path)
+
+"""
+If config not present, write the default
+"""
+if not file_path.is_file():
+    _write_config(DEFAULT_CONFIG)
+
+
+def get(key: str) -> Any:
+    """
+    Gets value of config from key
+
+    :param key: config key
+    :type key: str
+    :return: config value
+    """
+    _check_key(key)
+    config = _read_config()
+    return config.get(key, DEFAULT_CONFIG.get(key))
+
+
+def set(key: str, value: Any):
+    """
+    Sets key/value pair in config
+
+    :param key: config key
+    :type key: str
+    :param value: value to be set
+    :type value: Any
+    """
+    _check_key(key)
+    config = _read_config()
+    config[key] = value
+    _write_config(config)
---
+++ todayi/util/fs.py
@@ -0,0 +1,37 @@
+"""
+Module used to interact with filesystem.
+"""
+
+from pathlib import Path
+from typing import Union
+
+
+def path(fp: str) -> Path:
+    """
+    Gets path given within users' home
+    directory.
+
+    :param fp: file path to expand to users' home directory
+    :type fp: str
+    :return: Path
+    """
+    return Path(fp).expanduser()
+
+
+def _resolve_fp(fp: Union[str, Path]) -> Path:
+    if isinstance(fp, str):
+        fp = path(fp)
+    return fp
+
+
+def file_text(fp: Union[str, Path]) -> str:
+    """
+    Reads text from file.
+
+    :param fp: file path in string format to
+    """
+    return _resolve_fp(fp).read_text()
+
+
+def write_file(content: str, fp: Union[str, Path]):
+    return _resolve_fp(fp).write_text(content)
